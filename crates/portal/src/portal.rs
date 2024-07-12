bc7d321ae5fd19958af567593e721169efd7807b
tonychg <tonychg7@pm.me>
2024-07-12 10:36:35 +0200

    chore(package): create cargo workspace

--- crates/portal/src/portal.rs
+++ crates/portal/src/portal.rs
@@ -1,9 +1,17 @@
 mod map;
-mod portal;
 mod position;
 mod redirection;
 
 pub use crate::map::{Cell, Map};
-pub use crate::portal::Portal;
 pub use crate::position::Position;
 pub use crate::redirection::Redirection;
+
+pub struct Portal {
+    pub position: Position,
+}
+
+impl Portal {
+    pub fn new(position: Position) -> Portal {
+        Portal { position }
+    }
+}
