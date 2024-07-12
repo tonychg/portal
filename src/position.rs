19720cc6c3d6683260116dc8508c2f94eb43c2bd
tonychg <tonychg7@pm.me>
2024-07-12 07:36:10 +0200

    test(find_closest): fix invalid returned value

--- src/position.rs
+++ src/position.rs
@@ -39,7 +39,7 @@ impl Position {
     }
 }
 
-/* #[test]
+#[test]
 fn test_find_closest() {
     let positions = vec![
         Position::new(5, 5), // 10
@@ -47,7 +47,7 @@ fn test_find_closest() {
         Position::new(2, 4), // 6
     ];
     let origin = Position::new(0, 0);
-    assert_eq!(origin.find_closest(&positions), &[2]);
+    assert_eq!(origin.find_closest(&positions), (6, vec![2]));
 }
 
 #[test]
@@ -59,5 +59,5 @@ fn test_find_closest_with_multiple() {
         Position::new(4, 2), // 6
     ];
     let origin = Position::new(0, 0);
-    assert_eq!(origin.find_closest(&positions), &[2, 3]);
-} */
+    assert_eq!(origin.find_closest(&positions), (6, vec![2, 3]));
+}
