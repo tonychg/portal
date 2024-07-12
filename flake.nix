043e50c216b7433954026a4013324a2907fd2c7d
tonychg <tonychg7@pm.me>
2024-07-12 07:35:13 +0200

    chore(flake): use nix flake

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          overlays = [ (import rust-overlay) ];
+          pkgs = import nixpkgs {
+            inherit system overlays;
+          };
+
+          rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+
+          # compile-time deps
+          packages = with pkgs; [
+            curl
+            wget
+            pkg-config
+            rustToolchain
+          ];
+        in
+        with pkgs;
+        {
+          devShells.default = mkShell {
+            nativeBuildInputs = packages;
+          };
+        }
+      );
+}
+
